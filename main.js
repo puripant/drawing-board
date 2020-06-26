b03608784214c14c842702982a975567900c900c
Puripant <puripant@gmail.com>
2020-06-26 18:32:20 +0700

    Draw lines over image

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-let threshold = 60;
-let src, dst;
+let threshold = 70;
+let input, src, dst;
 
 let imgElement = document.getElementById('input')
 imgElement.onload = () => {
-  src = cv.imread('input');
+  input = cv.imread('input');
+  src = input.clone();
+  // cv.imshow('output', src);
   cv.cvtColor(src, src, cv.COLOR_RGBA2GRAY, 0);
   cv.Canny(src, src, 50, 200, 3);
-  cv.imshow('intermediate', src);
+  // cv.imshow('intermediate', src);
 
   update(threshold);
 }
@@ -23,7 +25,7 @@ inputElement.addEventListener('change', (e) => {
 // });
 
 function update(threshold) {
-  dst = cv.Mat.ones(src.rows, src.cols, cv.CV_8UC3);
+  dst = input.clone() //cv.Mat.ones(src.rows, src.cols, cv.CV_8UC3);
 
   findLines(threshold);
   findCircles(threshold/2);
@@ -43,7 +45,7 @@ function findLines(threshold) {
     let y0 = b * rho;
     let startPoint = {x: x0 - 1000 * b, y: y0 + 1000 * a};
     let endPoint = {x: x0 + 1000 * b, y: y0 - 1000 * a};
-    cv.line(dst, startPoint, endPoint, [255, 255, 255, 100]);
+    cv.line(dst, startPoint, endPoint, [100, 100, 100, 100]);
   }
   lines.delete();
 }
@@ -55,7 +57,8 @@ function findCircles(threshold) {
     let y = circles.data32F[i * 3 + 1];
     let radius = circles.data32F[i * 3 + 2];
     let center = new cv.Point(x, y);
-    cv.circle(dst, center, radius, [255, 255, 255, 100]);
+    cv.circle(dst, center, radius, [100, 100, 100, 100]);
   }
+  console.log(circles)
   circles.delete();
 }
